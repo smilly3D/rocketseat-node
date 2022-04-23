6e2136b31550db36a1d3fb1b325e176445bc4f49
smilly <smilly.3d@gmail.com>
2022-04-22 21:25:29 -0300

    feat: refactor folders, and imports

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,10 @@
 import { container } from "tsyringe";
 
-import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUserRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsReposiry } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationsReposiry } from "@modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 // ICategoriesRepository
